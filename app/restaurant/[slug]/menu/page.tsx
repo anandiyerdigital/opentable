eb2cc4a563fe3dfebb1d5c3ab9ff0ead37e8e267
Anand Iyer <aiyer@MacBook-Pro-2023---AI.local>
2023-02-21 13:50:01 -0500

    created components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,4 +1,12 @@
+'use client'
+
+import Header from '@/app/components/Header'
+import RestaurantCard from '@/app/components/RestaurantCard'
+import Link from 'next/link'
 import React from 'react'
+import Menu from '../components/Menu'
+import RestaurantHeader from '../components/RestaurantHeader'
+import RestaurantNavBar from '../components/RestaurantNavBar'
 
 const page = () => {
   return (
@@ -6,9 +14,7 @@ const page = () => {
   <main className="max-w-screen-2xl m-auto bg-white">
     {/* NAVBAR */}
     <nav className="bg-white p-2 flex justify-between">
-      <a href="" className="font-bold text-gray-700 text-2xl">
-        {" "} OpenTable{" "}
-      </a>
+    <Link href="/" className="font-bold text-gray-700 text-2xl"> OpenTable </Link>
       <div>
         <div className="flex">
           <button
@@ -21,42 +27,14 @@ const page = () => {
       </div>
     </nav>
     {/* NAVBAR */} {/* HEADER */}
-    <div className="h-96 overflow-hidden">
-      <div
-        className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center"
-      >
-        <h1 className="text-7xl text-white captitalize text-shadow text-center">
-          Milestones Grill (Toronto)
-        </h1>
-      </div>
-    </div>
+    <RestaurantHeader />
     {/* HEADER */} {/* DESCRIPTION PORTION */}
     <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
       <div className="bg-white w-[100%] rounded p-3 shadow">
         {/* RESAURANT NAVBAR */}
-        <nav className="flex text-reg border-b pb-2">
-          <a href="" className="mr-7"> Overview </a>
-          <a href="" className="mr-7"> Menu </a>
-        </nav>
+        <RestaurantNavBar />
         {/* RESAURANT NAVBAR */} {/* MENU */}
-        <main className="bg-white mt-5">
-          <div>
-            <div className="mt-4 pb-1 mb-1">
-              <h1 className="font-bold text-4xl">Menu</h1>
-            </div>
-            <div className="flex flex-wrap justify-between">
-              {/* MENU CARD */}
-              <div className=" border rounded p-3 w-[49%] mb-3">
-                <h3 className="font-bold text-lg">Surf and Turf</h3>
-                <p className="font-light mt-1 text-sm">
-                  A well done steak with lobster and rice
-                </p>
-                <p className="mt-7">$80.00</p>
-              </div>
-              {/* MENU CARD */}
-            </div>
-          </div>
-        </main>
+        <Menu />
         {/* MENU */}
       </div>
     </div>
